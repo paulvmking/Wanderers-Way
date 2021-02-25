b245499a6b7226c2c9a0df4d0341517f57668c6a
paulvmking <paulvmking@gmail.com>
2021-02-25 16:32:17 +0000

    added links to navbar for all pages so far

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
-sunFunction() {
-    location.href = "sun.html";
+document.getElementById("sun-1").onclick = function () {
+        location.href = "sun.html";
 }
 
-snowFunction() {
-    location.href = "snow.html";
+document.getElementById("snow-1").onclick = function () {
+        location.href = "snow.html";
 }
 
 
