32001639793571d0eed06278cfb125d635a6d81f
paulvmking <paulvmking@gmail.com>
2021-02-26 15:02:38 +0000

    added opacity effect to choice images

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,8 +8,27 @@ function sunFunction() {
     location.href = 'sun.html';
 }
 
+// To give user feedback via opacity change with mouseenter/mouseleave events
 
+let sunEffect1 = document.getElementById("sun-1");
 
+sunEffect1.addEventListener("mouseover", function() {
+    sunEffect1.style.opacity = 0.8;
+});
+
+sunEffect1.addEventListener("mouseout", function() {
+    sunEffect1.style.opacity = 1.0;
+});
+
+let snowEffect1 = document.getElementById("snow-1");
+
+snowEffect1.addEventListener("mouseover", function() {
+    snowEffect1.style.opacity = 0.8;
+});
+
+snowEffect1.addEventListener("mouseout", function() {
+    snowEffect1.style.opacity = 1.0;
+});
 
 
 
