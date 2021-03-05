8da8f06fff81c75d0d5b3a11fca4b74c2dadfe81
paulvmking <paulvmking@gmail.com>
2021-03-05 19:37:09 +0000

    changed button styles to provide better functionality with bootstrap

--- assets/js/script.js
+++ assets/js/script.js
@@ -80,7 +80,8 @@ for(var i = 0; i < sunText.length; i += 1) {
     div.className = "sun-card" + i;
     div.id = "card" + i;
     div.innerHTML =  `<img class="card-img-top" src="${sunText[i].image}" alt="Card image cap"><div class="card-body"><h5 class="card-title">${sunText[i].name}</h5><p class="card-text">${sunText[i].price}</p></div>`;
-    document.body.appendChild(div);
+    let main = document.getElementById("main");
+    main.appendChild(div);
 }
 }
 
