84113ad91ad029bdd96296e6e3b027cd89940673
paulvmking <paulvmking@gmail.com>
2021-03-09 19:54:25 +0000

    added names to fields to be filled and sent in email to register

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -2,6 +2,7 @@ function sendMail(registerForm) {
     emailjs.send("gmail", "wanderers_way", {
         "from_email": registerForm.emailaddress.value,
         "from_email_confirmed": registerForm.emailaddressConfirmed.value,
+        "email_extra": registerForm.checkBox.value
     })
     .then(
         function(response) {
