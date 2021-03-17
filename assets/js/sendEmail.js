d66a745fbdc2efb8d4601a15f56b13e00011648a
paulvmking <paulvmking@gmail.com>
2021-03-17 17:51:04 +0000

    added contact form details to work with email.js and tested as successful

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -13,3 +13,20 @@ function sendMail(registerForm) {
     );
     return false;
 }
+
+function sendMail(contactForm) {
+    emailjs.send("gmail", "contact-form", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "message":contactForm.message.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+    return false;
+}
