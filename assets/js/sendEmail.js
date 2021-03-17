c45f9bccd6c226118881f809cc0a127382832cbb
paulvmking <paulvmking@gmail.com>
2021-03-17 18:04:18 +0000

    added clear form to sendmail function to clear upon entry

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -3,30 +3,16 @@ function sendMail(registerForm) {
         "from_email": registerForm.emailaddress.value,
         "from_email_confirmed": registerForm.emailaddressConfirmed.value,
     })
-    .then(
-        function(response) {
-            console.log("SUCCESS", response);
-        },
-        function(error) {
-            console.log("FAILED", error);
-        }
-    );
     return false;
 }
 
 function sendMail(contactForm) {
+    var form = document.getElementById('contact-form');
     emailjs.send("gmail", "contact-form", {
         "from_name": contactForm.name.value,
         "from_email": contactForm.emailaddress.value,
         "message":contactForm.message.value
     })
-    .then(
-        function(response) {
-            console.log("SUCCESS", response);
-        },
-        function(error) {
-            console.log("FAILED", error);
-        }
-    );
+    form.reset();
     return false;
-}
+    }
