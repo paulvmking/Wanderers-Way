606bef331a4a3c6e592fb7acce2689d335d8ff00
paulvmking <paulvmking@gmail.com>
2021-03-18 15:02:34 +0000

    added comments and beautified code

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,67 +7,67 @@ function loadWelcome() {
 
     if (hourNow > 18) {
         message = 'Good Evening!';
-    }else if (hourNow > 12) {
+    } else if (hourNow > 12) {
         message = 'Good Afternoon!';
-    }else if (hourNow > 0) {
+    } else if (hourNow > 0) {
         message = 'Good Morning!';
-    }else {
+    } else {
         message = 'Welcome!';
     }
 
     let hello = document.createElement('h1');
-    hello.innerHTML = ( message +  "<br><hr></br>" + "Which way shall you wander?");
+    hello.innerHTML = (message + "<br><hr></br>" + "Which way shall you wander?");
     let greeting = document.getElementById("greeting-question");
     greeting.appendChild(hello);
 }
 
 // To redirect user to appropriate page once choice is clicked
 
-function sunChosen(){
-let destinationUrl = "destinations.html?weather=sun";
-const sunChosen = document.getElementById("sunCard");
-sunChosen.addEventListener("click", function() {
-    if (parent.hostname === "paulvmking.github.io"){
-        parent.location.href = destinationUrl;
-    }else {
-        location.href = destinationUrl;
-    }
-});
+function sunChosen() {
+    let destinationUrl = "destinations.html?weather=sun";
+    const sunChosen = document.getElementById("sunCard");
+    sunChosen.addEventListener("click", function () {
+        if (parent.hostname === "paulvmking.github.io") {
+            parent.location.href = destinationUrl;
+        } else {
+            location.href = destinationUrl;
+        }
+    });
 }
 
-function snowChosen(){
-const destinationUrl = "destinations.html?weather=snow";
-const snowChosen = document.getElementById("snowCard");
-snowChosen.addEventListener("click", function() {
-    if (parent.hostname === "paulvmking.github.io"){
-        parent.location.href = destinationUrl;
-    }else {
-        location.href = destinationUrl;
-    }
-});
+function snowChosen() {
+    const destinationUrl = "destinations.html?weather=snow";
+    const snowChosen = document.getElementById("snowCard");
+    snowChosen.addEventListener("click", function () {
+        if (parent.hostname === "paulvmking.github.io") {
+            parent.location.href = destinationUrl;
+        } else {
+            location.href = destinationUrl;
+        }
+    });
 }
 
 // To give user feedback via opacity change with mouseover/mouseout events
 
-function makeOpacity(){
-const sunCard = document.getElementById("sunCard");
-const snowCard = document.getElementById("snowCard");
-const mouseEvent = (card, action) => card.style.opacity = action === "mouseover" ? 0.7 : 1.0 ;
+function makeOpacity() {
+    const sunCard = document.getElementById("sunCard");
+    const snowCard = document.getElementById("snowCard");
+    const mouseEvent = (card, action) => card.style.opacity = action === "mouseover" ? 0.7 : 1.0;
 
-sunCard.addEventListener("mouseover",() =>  mouseEvent(sunCard, "mouseover"));
-sunCard.addEventListener("mouseout", () => mouseEvent(sunCard, "mouseout"));
-snowCard.addEventListener("mouseover", () => mouseEvent(snowCard, "mouseover"));
-snowCard.addEventListener("mouseout", () => mouseEvent(snowCard, "mouseout"));
+    sunCard.addEventListener("mouseover", () => mouseEvent(sunCard, "mouseover"));
+    sunCard.addEventListener("mouseout", () => mouseEvent(sunCard, "mouseout"));
+    snowCard.addEventListener("mouseover", () => mouseEvent(snowCard, "mouseover"));
+    snowCard.addEventListener("mouseout", () => mouseEvent(snowCard, "mouseout"));
 }
 
 //To add and removed current class styling when buttons are clicked
 
-$('#myBtnContainer').on('click', '.myBtn', function() {
-  $(this).addClass('current').siblings().removeClass('current');
+$('#myBtnContainer').on('click', '.myBtn', function () {
+    $(this).addClass('current').siblings().removeClass('current');
 });
+
+// To provide user alert when registration form is successfully sent
 
 document.getElementById('register-modal').addEventListener('submit', function () {
     alert("Thank you for registering!  Please await a reply to continue to your bookings!");
-},false);
-
-
+}, false);
