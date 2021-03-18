8e81dddade1b0e990590cf70e3a37c420de5818e
paulvmking <paulvmking@gmail.com>
2021-03-18 14:46:10 +0000

    added comments to file and beautified

--- assets/js/destinations.js
+++ assets/js/destinations.js
@@ -1,26 +1,29 @@
+// To filter what cards are shown depending on the url of the current page and what category sun or snow was clicked
+
 const destinationsDiv = document.getElementById('destinations');
 const queryParam = window.location.search;
 const weather = new URLSearchParams(queryParam.slice(1)).getAll('weather')[0];
 
 const renderDestinations = () => {
-  const destinationsByWeather = locations.filter(loca => loca.weather === weather);
+    const destinationsByWeather = locations.filter(loca => loca.weather === weather);
 
-  let innerDestination = '';
+    let innerDestination = '';
 
-  destinationsByWeather.map(dest => {
-    return innerDestination = `${innerDestination}<a class="hover ${dest.name}-card" href="destination.html?destination=${dest.name}"><img class="card-img-top" src="${dest.image}" alt="Card image cap"><div class="card-body"><h4 class="card-title">${dest.name}</h4><p class="card-text">${dest.price}</p></div></a>`;
-  });
+    destinationsByWeather.map(dest => {
+        return innerDestination = `${innerDestination}<a class="hover ${dest.name}-card" href="destination.html?destination=${dest.name}"><img class="card-img-top" src="${dest.image}" alt="Card image cap"><div class="card-body"><h4 class="card-title">${dest.name}</h4><p class="card-text">${dest.price}</p></div></a>`;
+    });
 
-  destinationsDiv.innerHTML = innerDestination
-}
+    destinationsDiv.innerHTML = innerDestination;
+};
 
 renderDestinations();
-// to show only selected budget locations after button click 
 
-function showBudget(category){
-    if (category === 'all'){
+// To show only selected budget locations after button click 
+
+function showBudget(category) {
+    if (category === 'all') {
         locations.map((location) => $(`.${location.name}-card`).toggle(true));
-    }else {
+    } else {
         let selectedLocation = locations.filter((location) => location.budget === category);
         let nonSelectedLocation = locations.filter((location) => location.budget !== category);
 
@@ -30,4 +33,3 @@ function showBudget(category){
 }
 
 
- 
