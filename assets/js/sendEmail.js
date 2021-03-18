79495a69f351e47a09717084ce1ad02fdf6db1f7
paulvmking <paulvmking@gmail.com>
2021-03-18 15:04:13 +0000

    added comment and beautified code

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,7 +1,9 @@
+// To send mail from register form modal
+
 function sendMail(registerForm) {
     emailjs.send("gmail", "wanderers_way", {
         "from_email": registerForm.emailaddress.value,
         "from_email_confirmed": registerForm.emailaddressConfirmed.value,
-    })
+    });
     return false;
 }
