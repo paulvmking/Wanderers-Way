8aab5dc9b2736a34056b9bc69cf4eb281b8fcbd8
paulvmking <paulvmking@gmail.com>
2021-03-18 15:05:59 +0000

    added comments and beautified code

--- assets/js/sendEmailContact.js
+++ assets/js/sendEmailContact.js
@@ -1,13 +1,17 @@
+// To send user data upon completion of contact form 
+
 function sendMail(contactForm) {
     emailjs.send("gmail", "contact-form", {
         "from_name": contactForm.name.value,
         "from_email": contactForm.emailaddress.value,
-        "message":contactForm.message.value
-    })  
+        "message": contactForm.message.value
+    });
     return false;
-    }
+}
+
+// To provide user alert upon completion of form
 
 document.getElementById('sendContact')
-	.addEventListener('click', function() {
-		alert('Thanks for contacting us!Please await an email to continue!');
-	});
+    .addEventListener('click', function () {
+        alert('Thanks for contacting us!Please await an email to continue!');
+    });
