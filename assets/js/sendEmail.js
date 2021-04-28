e58d4c8d4a6a9113a73f2874d04765d350605f8a
paulvmking <paulvmking@gmail.com>
2021-04-28 19:27:21 +0000

    removed email confirm info being sent to email address as single email address is sufficient

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -3,7 +3,6 @@
 function sendMail(registerForm) {
   emailjs.send("gmail", "wanderers_way", {
     from_email: registerForm.emailaddress.value,
-    from_email_confirmed: registerForm.emailaddressConfirmed.value,
   });
   return false;
 }
